153eddf3ac9329e2433e208f95d695d5e9987cde
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-11-23 01:11:51 +0300

    modified print integer

--- print_integer.c
+++ print_integer.c
@@ -1,4 +1,42 @@
 #include "main.h"
+
+/**
+ * number - is a number signed or not
+ * @args: List of arguments
+ * Return: The number of arguments printed
+ */
+int number(va_list args)
+{
+	int n;
+	int div;
+	int arg_len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	div = 1;
+	arg_len = 0;
+
+	if (n < 0)
+	{
+		arg_len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		arg_len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (arg_len);
+}
+
 /**
  * print_integer - Prints integer
  * @list: list of arguments
@@ -8,6 +46,6 @@ int print_integer(va_list list)
 {
 	int num_length;
 
-	num_length = print_number(list);
+	num_length = number(list);
 	return (num_length);
 }
